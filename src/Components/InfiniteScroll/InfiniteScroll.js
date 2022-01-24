48fd22aa9fad0df2c47b0b434e5fdeb2ef5473aa
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-24 10:48:20 +0100

    create UI

--- src/Components/InfiniteScroll/InfiniteScroll.js
+++ src/Components/InfiniteScroll/InfiniteScroll.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import './InfiniteScroll.css'
+import React, { useState, useEffect, useRef } from "react";
+import "./InfiniteScroll.css";
+import { v4 as uuidv4 } from "uuid";
 
 export default function InfiniteScroll() {
-  return (
-    <div>
+    
+  const [dataImg, setDataImg] = useState([[], [], []]);
+  const [pageIndex, setPageIndex] = useState(1);
+  const [searchState, setSearchState] = useState("random");
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+
+  const inputRef = useRef();
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSearch}>
+        <label htmlFor="search">Votre recherche</label>
+        <input type="text" id="search" ref={inputRef} />
+      </form>
+      <div className="card-list">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 }
-
-
